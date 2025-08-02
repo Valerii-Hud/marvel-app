be2cc3055d5a5f4f107a9e4430426c10f48ed86f
Valerii Hud <support@valerii-hud.com>
2025-08-02 14:56:15 +0200

    feat: added random chars list

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,77 @@
+import React, { Component } from 'react';
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
-const CharList = () => {
+class CharCard extends Component {
+  render() {
+    const { thumbnail, name } = this.props;
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+      <li className="char__item">
+        <img src={thumbnail} alt={name} />
+        <div className="char__name">{name}</div>
+      </li>
+    );
+  }
+}
+
+class CharList extends Component {
+  state = {
+    chars: [],
+    loading: true,
+    error: false,
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.loadCharacters();
+  }
+
+  getRandomId = () => Math.floor(Math.random() * 20) + 1;
+
+  loadCharacters = async () => {
+    const requests = Array.from({ length: 9 }, () =>
+      this.marvelService.getCharacter(this.getRandomId())
+    );
+
+    try {
+      const chars = await Promise.all(requests);
+      this.setState({ chars, loading: false, error: false });
+    } catch (e) {
+      this.setState({ loading: false, error: true });
+    }
+  };
+
+  render() {
+    const { chars, loading, error } = this.state;
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+
+    const content = !(loading || error) ? (
+      <ul className="char__grid">
+        {chars.map((char) => (
+          <CharCard key={char.id} name={char.name} thumbnail={char.thumbnail} />
+        ))}
+      </ul>
+    ) : null;
+
+    return (
+      <div className="char__list">
+        {errorMessage}
+        {spinner}
+        {content}
+        <button
+          className="button button__main button__long"
+          onClick={this.loadCharacters}
+        >
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default CharList;
